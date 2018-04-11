be4a23e013a4ebbc6ad8bdcdb8381957423495f2
Chaos John <chaosjohn.yjh@icloud.com>
2018-04-11 23:10:40 +0800

    create Functions and Closures

---
+++ Playground2018/GuidedTour/Functions and Closures.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)"
+}
+print(greet(person: "Bob", day: "Tuesday"))
